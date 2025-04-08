f7d039fd4fd1afffc7c9694b3e6047d7c294c994
Jahanvi Sharma <789jahanvi@gmail.com>
2025-04-09 01:28:44 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+
+app.get("/",(req, res)=>{
+    res.send("Hello World");
+});
+
+export {app};
